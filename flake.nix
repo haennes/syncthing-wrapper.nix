2796c1aa204a5da7fbe91a15d9409146cb395f7b
haennes <hannes.hofmuth@gmail.com>
2024-10-06 20:04:34 +0200

    fmt

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
     "A simple to use syncthing wrapper. Declare folders once roll out to all automagically";
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
-  outputs = {...}:{
-      nixosModules = rec {
-        syncthing-wrapper = import ./syncthing-wrapper.nix;
-        default = syncthing-wrapper;
-      };
+  outputs = { ... }: {
+    nixosModules = rec {
+      syncthing-wrapper = import ./syncthing-wrapper.nix;
+      default = syncthing-wrapper;
     };
+  };
 
 }
