44102e8a3feacac891a004b018968de168c4d19f
haennes <hannes.hofmuth@gmail.com>
2024-10-06 20:22:13 +0200

    better example that actually builds

--- example/example.nix
+++ example/example.nix
@@ -1,64 +1,69 @@
-  {}:{
-    services.syncthing_wrapper = rec {
-      default_versioning = {
-        type = "simple";
-        params.keep = "10";
-      };
-      devices = rec {
-        all_pcs = {
-          thinkpad = ids.thinkpad;
-          thinknew = ids.thinknew;
-          mainpc = ids.mainpc;
-        };
-        all_handys = {
-          handy = ids.handy;
-
-        };
-        servers = { syncschlawiner = ids.syncschlawiner; };
-        all_servers = servers // { tabula = ids.tabula; };
-
-      };
-      folders = with devices;
-        with devices.all_handys;
-        with devices.all_servers; {
-          "Family" = {
-            devices = (all_pcs // servers);
-            paths = {
-              "mainpc" = "/home/Family";
-              "thinkpad" = "/home/Family";
-              "thinknew" = "/home/Family";
-            };
-          };
-          "Passwords" = {
-            devices = (all_pcs // all_handys // servers);
-            versioning = {
-              type = "simple";
-              params.keep = "100";
-            };
-          };
-          "3d_printing" = [ (all_pcs // servers) ];
-          "Documents" = [ (all_pcs // servers) ];
-          "Notes" = [ (all_pcs // servers) ];
-          "Downloads" = [ (all_pcs // servers) ];
-          "Studium" = {
-          devices = [ (all_pcs // uni)  ];
-          paths = rec {
-            "mainpc" = "/home/hannses/Documents/Studium/Semester1";
-            "thinkpad" = mainpc;
-            "thinknew" = mainpc;
-          };
-          };
-        };
+{ ... }:
+let ids = import ./ids.nix;
+in {
+  networking.hostName = "pcA"; # alternatively set dev_name in syncthing_wrapper
+  boot.isContainer = true; # Hack to have an easy time building
+  services.syncthing_wrapper = rec {
+    enable = true;
+    default_versioning = {
+      type = "simple";
+      params.keep = "10";
     };
+    devices = rec {
+      all_pcs = {
+        pcA = ids.pcA;
+        pcB = ids.pcB;
+      };
+      all_mobiles = {
+        mobile = ids.mobile;
 
-    services.syncthing = {
-      settings = {
-        options = {
-          urAccepted = -1; # do not send reports
-          relaysEnabled = true;
+      };
+      servers = { serverA = ids.serverA; };
+      all_servers = servers // { tabula = ids.tabula; };
+
+    };
+    folders = with devices; {
+      Family = {
+        devices = (all_pcs // servers);
+        paths = {
+          pcA = "/home/Family";
+          pcB = "/home/Family";
         };
       };
-      key = lib.mkIf(config.services.syncthing.enable) config.age.secrets."syncthing_key_${config.networking.hostName}".path;
-      cert = lib.mkIf(config.services.syncthing.enable) config.age.secrets."syncthing_cert_${config.networking.hostName}".path;
+      Passwords = {
+        devices = (all_pcs // all_mobiles // servers);
+        versioning = {
+          type = "simple";
+          params.keep = "100";
+        };
+      };
+      Documents = [ (all_pcs // servers) ];
+      subdir = {
+        devices = [ all_pcs "pcA" ];
+        paths = rec {
+          pcA = "/home/user/Documents/subdir";
+          pcB = pcA;
+        };
+      };
     };
+  };
+
+  services.syncthing = {
+    openDefaultPorts = true;
+    dataDir = "/tmp";
+    settings = {
+      options = {
+        urAccepted = -1; # do not send reports
+        relaysEnabled = true;
+      };
+    };
+    # some values just to make eval happy :)
+    key = ./key;
+    cert = ./cert;
+    # this is how have it in my config, all hosts import a the same module !!
+    #key = lib.mkIf (config.services.syncthing.enable)
+    #  config.age.secrets."syncthing_key_${config.networking.hostName}".path;
+    #cert = lib.mkIf (config.services.syncthing.enable)
+    #  config.age.secrets."syncthing_cert_${config.networking.hostName}".path;
+  };
 }
--- flake.nix
+++ flake.nix
@@ -3,10 +3,14 @@
     "A simple to use syncthing wrapper. Declare folders once roll out to all automagically";
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
-  outputs = { ... }: {
+  outputs = { nixpkgs, self, ... }: {
     nixosModules = rec {
       syncthing-wrapper = import ./syncthing-wrapper.nix;
       default = syncthing-wrapper;
+    };
+    nixosConfigurations.pcA = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./example/example.nix self.nixosModules.syncthing-wrapper ];
     };
   };
 
