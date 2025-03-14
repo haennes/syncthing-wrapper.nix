09de7a601f60d08f339e306c137a702d38081dc0
haennes <hannes.hofmuth@gmail.com>
2025-03-14 15:45:50 +0100

    easier debugging

---
+++ print_short.nix
@@ -0,0 +1,28 @@
+#this file is for debugging purposes
+#usage: load a nix repl with the example config
+# > :l print_short.nix
+# > f = meta inputs
+# now you are ready to start the :lf and eval cycle ;)
+# > :lf .
+# > f.bindfs nixosConfigurations
+# FAQ: why dont we add cfg as an argument of meta -> :lf won't update the cfg parameter
+{
+  meta =
+    inputs:
+    let
+      lib = inputs.nixpkgs.lib;
+      inherit (lib) mapAttrsToList concatStringsSep;
+    in
+    {
+      bindfs =
+        cfg:
+        lib.mapAttrsToList (
+          n: v:
+          "${n} \t ${v.source} \t ${concatStringsSep "" (mapAttrsToList (n: v: "${n}-${v}") v.map.userGroup)}"
+        ) cfg.pcA.config.bindfs.folders;
+      syncf = cfg: cfg.pcA.config.services.syncthing.settings.folders;
+      syncwf = cfg: cfg.pcA.config.services.syncthing-wrapper.folders;
+      syncd = cfg: cfg.pcA.config.services.syncthing.settings.folders;
+
+    };
+}
