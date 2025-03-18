f7199458e9743315507fa272e1b56813cbc0a671
haennes <hannes.hofmuth@gmail.com>
2025-03-18 08:46:56 +0100

    fix eval activationScript

--- config.nix
+++ config.nix
@@ -95,75 +95,95 @@ in
 
     };
     system.activationScripts.syncthing-wrapper-ensure =
-    let
-      inherit (lib) optional optionalString;
-      ensureDirExistsGen = attr: optional attr.ensure.DirExists "mkdir -p ${attr.path}";
-      ensureOwner = attr: owner:
       let
-        owner_opt = attr.ensure.owner;
+        inherit (lib) optional optionalString;
+        ensureDirExistsGen =
+          attr: optional (attr.ensure.DirExists && attr.path != null) "mkdir -p ${attr.path}";
+        ensureOwner =
+          attr: owner:
+          let
+            owner_opt = attr.ensure.owner;
+          in
+          optional (
+            owner_opt.owner && attr.path != null
+          ) "chown ${optionalString owner_opt.recursive "-R"} ${owner} ${attr.path}";
+        ensureGroup =
+          attr: group:
+          let
+            group_opt = attr.ensure.group;
+          in
+          optional (
+            group_opt.group && attr.path != null
+          ) "chown ${optionalString group_opt.recursive "-R"} :${group} ${attr.path}";
+        ensurePermissions =
+          attr:
+          let
+            permissions_opt = attr.ensure.permissions;
+          in
+          optional (permissions_opt.permissions != null && attr.path != null)
+            "chmod ${optionalString permissions_opt.recursive "-R"} ${permissions_opt.permissions} ${attr.path}";
       in
-      optional owner_opt.owner "chown ${optionalString owner_opt.recursive "-R"} ${owner} ${attr.path}";
-      ensureGroup = attr: group:
-      let
-        group_opt = attr.ensure.group;
-      in
-      optional group_opt.group "chown ${optionalString group_opt.recursive "-R"} :${group} ${attr.path}";
-      ensurePermissions = attr:
-      let
-        permissions_opt = attr.ensure.permissions;
-      in
-      optional (permissions_opt.permissions != null) "chmod ${optionalString permissions_opt.recursive "-R"} ${permissions_opt.permissions} ${attr.path}";
-    in
       lib.concatLines (
-      (ensureDirExistsGen cfg.paths.physicalPath)
-      #exist
-      ++ (ensureDirExistsGen cfg.paths.basePath)
-      ++ (ensureDirExistsGen cfg.paths.users.defaultUserDir)
-      ++ (flatten (mapAttrsToList(_: folder: mapAttrsToList(_: dir: ensureDirExistsGen dir) folder.path.${hostname}) folders))
+        (ensureDirExistsGen cfg.paths.physicalPath)
+        #exist
+        ++ (ensureDirExistsGen cfg.paths.basePath)
+        ++ (ensureDirExistsGen cfg.paths.users.defaultUserDir)
+        ++ (flatten (
+          mapAttrsToList (
+            _: folder: mapAttrsToList (_: dir: ensureDirExistsGen dir) folder.path.${hostname}
+          ) folders
+        ))
 
-      #owner & group
-      ##physicalPath
-      ++ (ensureOwner cfg.paths.physicalPath cfg_s.user)
-      ++ (ensureGroup cfg.paths.physicalPath cfg_s.group)
+        #owner & group
+        ##physicalPath
+        ++ (ensureOwner cfg.paths.physicalPath cfg_s.user)
+        ++ (ensureGroup cfg.paths.physicalPath cfg_s.group)
 
-      ##basePath
-      ++ (ensureOwner cfg.paths.basePath cfg.paths.basePath.ensure.owner.name)
-      ++ (ensureGroup cfg.paths.basePath cfg.paths.basePath.ensure.group.name)
+        ##basePath
+        ++ (ensureOwner cfg.paths.basePath cfg.paths.basePath.ensure.owner.name)
+        ++ (ensureGroup cfg.paths.basePath cfg.paths.basePath.ensure.group.name)
 
-      ##defaultUserDir
-      ++ (ensureOwner cfg.paths.users.defaultUserDir cfg.paths.users.defaultUserDir.ensure.owner.name)
-      ++ (ensureGroup cfg.paths.users.defaultUserDir cfg.paths.users.defaultUserDir.ensure.group.name)
+        ##defaultUserDir
+        ++ (ensureOwner cfg.paths.users.defaultUserDir cfg.paths.users.defaultUserDir.ensure.owner.name)
+        ++ (ensureGroup cfg.paths.users.defaultUserDir cfg.paths.users.defaultUserDir.ensure.group.name)
 
-      ##userFolders
-      ++ (flatten(
-        mapAttrsToList( _: v:
-        let
-          system_paths = v.path.${hostname};
+        ##userFolders
+        ++ (flatten (
+          mapAttrsToList (
+            _: v:
+            let
+              system_paths = v.path.${hostname};
               target_paths = removeAttrs system_paths [ "system" ];
             in
-          mapAttrsToList(
-            username: target_path:
-            (ensureOwner target_path username)
-            ++ lib.optionals (lib.hasAttrByPath [username "group"] config.users.users) (ensureGroup target_path config.users.users.${username}.group)
-          ) target_paths
-        ) folders
-      ))
+            mapAttrsToList (
+              username: target_path:
+              (ensureOwner target_path username)
+              ++ lib.optionals (lib.hasAttrByPath [ username "group" ] config.users.users) (
+                ensureGroup target_path config.users.users.${username}.group
+              )
+            ) target_paths
+          ) folders
+        ))
 
-      ##physicalFolder
-      ++ (flatten(
-        mapAttrsToList(_: v:
+        ##physicalFolder
+        ++ (flatten (
+          mapAttrsToList (
+            _: v:
             (ensureOwner v.path.${hostname}.system cfg_s.user)
             ++ (ensureGroup v.path.${hostname}.system cfg_s.group)
-        ) folders
-      ))
-      #permissions
-      ++ (ensurePermissions cfg.paths.physicalPath)
-      ++ (ensurePermissions cfg.paths.basePath)
-      ++ (ensurePermissions cfg.paths.users.defaultUserDir)
-      ++ (flatten (mapAttrsToList(_: folder: mapAttrsToList(_: dir: ensurePermissions dir) folder.path.${hostname}) folders))
+          ) folders
+        ))
+        #permissions
+        ++ (ensurePermissions cfg.paths.physicalPath)
+        ++ (ensurePermissions cfg.paths.basePath)
+        ++ (ensurePermissions cfg.paths.users.defaultUserDir)
+        ++ (flatten (
+          mapAttrsToList (
+            _: folder: mapAttrsToList (_: dir: ensurePermissions dir) folder.path.${hostname}
+          ) folders
+        ))
 
-      )
-    ;
+      );
 
     boot.kernel.sysctl = lib.mkIf (cfg.fsNotifyWatches != null) {
       "fs.inotify.max_user_watches" = cfg.fsNotifyWatches;
